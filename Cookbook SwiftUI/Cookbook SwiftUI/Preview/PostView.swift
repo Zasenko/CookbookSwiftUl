f87488df1edcb323af031f4fb704f4eb4f4773d3
Zasenko <dmitry.zasenko@gmail.com>
2024-05-24 21:25:09 +0200

    Preview

---
+++ Cookbook SwiftUI/Cookbook SwiftUI/Preview/PostView.swift
@@ -0,0 +1,18 @@
+//
+//  PostView.swift
+//  Cookbook SwiftUI
+//
+//  Created by Dmitry Zasenko on 24.05.24.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostView()
+}
